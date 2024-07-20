26cbcfa5725c255d6cc812a67e18871571a8251a
Iftekhar-w <mdiftekharsalam@gmail.com>
2024-07-20 15:16:45 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://e7.pngegg.com/pngimages/498/917/png-clipart-computer-icons-desktop-chatbot-icon-blue-angle.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://e7.pngegg.com/pngimages/498/917/png-clipart-computer-icons-desktop-chatbot-icon-blue-angle.png" style="max-height: 41px; max-width: 41px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
