828a1e78026602fc0363e7baed0e00236f8d6bb5
Iftekhar-w <mdiftekharsalam@gmail.com>
2024-07-28 00:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,11 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
-# from pathlib import Path
 import filetype
 
 from streamlit.type_util import BytesLike
 api_key=os.environ.get('GOOGLE_API_KEY')
 
-
-# def get_pdf_text(pdf_docs):
-#     text = ""
-#     for pdf in pdf_docs:
-#         pdf_reader = PdfReader(pdf)
-#         for page in pdf_reader.pages:
-#             text += page.extract_text()
-#     return text
 
 def get_pdf_text(pdf_docs):
     text = ""
