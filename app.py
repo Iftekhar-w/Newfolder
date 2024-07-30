e2b095a62f84a4f3da70feb2b6d409bc645d5186
Iftekhar-w <mdiftekharsalam@gmail.com>
2024-07-30 19:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 from streamlit.type_util import BytesLike
 # api_key=st.secrets['GOOGLE_API_KEY']
 load_dotenv()
-api_key=os.getenv['GOOGLE_API_KEY']
+api_key=os.environ['GOOGLE_API_KEY']
 
 
 def get_pdf_text(pdf_docs):
